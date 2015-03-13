aca2a71dd1e2e9b467e91e4b4234076ae930fa7d
nonnycode <miknonny@gmail.com>
2015-03-13 14:05:26 +0100

    added store with setInterval trigger

--- scripts/App.js
+++ scripts/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+var React = require('react');
+var Reflux = require('reflux');
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
-}
+var store =   Reflux.createStore({
+  data: {message: 0},
+  
+  init() {
+      setInterval(()=>{
+        this.data.message++
+        this.trigger(this.data);
+      }, 2000);
+    },
+  getInitialState() {
+      return this.data;
+     },
+})
+
+
+var App = 
+  React.createClass({
+    mixins: [Reflux.connect(store)],
+
+    render() {
+      return (<h1>{this.state.message}</h1>);
+    }
+  });
+
+module.exports = App;
